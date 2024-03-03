614e3028f3fb4349bd7c814956f7f81878fce9eb
nabilabanati <158720490+nabilabanati@users.noreply.github.com>
2024-03-03 21:58:08 +0700

    Add files via upload

---
+++ Unit 1/latihan-unit1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+    int s, t, v;
+
+    printf ("Masukan jarak dan waktu : ");
+    scanf ("%d %d", &s, &t);
+    float kecepatan = (float) s / t;
+
+    printf ("Masukan kecepatan dan waktu : ");
+    scanf ("%d %d", &v, &t);
+    float jarak = (float) v * t;
+
+    printf ("Masukan jarak dan kecepatan : ");
+    scanf ("%d %d", &s, &v);
+    float waktu = (float) s / v;
+
+    printf ("\nKecepatan:  %.2f\n", kecepatan);
+    printf ("Jarak: %.2f\n", jarak);
+    printf ("Waktu: %.2f\n", waktu);
+
+    return 0;
+
+}
+
